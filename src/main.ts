0dcd176ff651b6c9e8bc1f17f16fc2a1b016043d
KGESH <baram789456@gmail.com>
2022-04-29 15:13:09 +0900

    env 모듈 설치

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { event_gateway_port, gateway_host, rest_gateway_port } from './config';
+import {
+  device_host,
+  event_gateway_port,
+  gateway_host,
+  rest_gateway_port,
+} from './config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Transport } from '@nestjs/microservices';
 import { ValidationPipe } from '@nestjs/common';
@@ -41,7 +46,7 @@ async function bootstrap() {
   SwaggerModule.setup('api-spec', app, document);
 
   console.log(
-    `geteway ENV - rest host${gateway_host}, rest PORT ${rest_gateway_port}`,
+    `geteway ENV - rest host ${gateway_host}, rest PORT ${rest_gateway_port}, device HOST ${device_host}`,
   );
   await app.listen(rest_gateway_port);
 }
