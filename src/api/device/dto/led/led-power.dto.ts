557e263281cdb43e9b8518846936293e013920a0
KGESH <baram789456@gmail.com>
2022-05-29 14:43:19 +0900

    Pre fetch

--- src/api/device/dto/led/led-power.dto.ts
+++ src/api/device/dto/led/led-power.dto.ts
@@ -1,15 +1,3 @@
-import { IsIn, IsNumber, IsString } from 'class-validator';
+import { SlavePowerDto } from '../slave/slave-power.dto';
 
-export class LedPowerDto {
-  @IsNumber()
-  private masterId: number;
-
-  @IsNumber()
-  private slaveId: number;
-
-  @IsString()
-  @IsIn(['on', 'off'], {
-    message: `'powerState' is not 'on' or 'off'`,
-  })
-  private powerState: string;
-}
+export class LedPowerDto extends SlavePowerDto {}
