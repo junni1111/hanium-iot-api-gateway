dcd1709f4e82e8aba32f9edf5d075c46761bfb88
김재한 <kimjaehan@gimjaehan-ui-MacBookAir.local>
2022-07-17 23:06:59 +0900

    cors 문제때문에 추가

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,12 @@ import { setupSwagger } from './util/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
