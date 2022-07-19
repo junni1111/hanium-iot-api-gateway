10d807d8fed51294975d272f855979758a6cde60
김재한 <kimjaehan@gimjaehan-ui-MacBookAir.local>
2022-07-19 17:07:25 +0900

    cors

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,6 @@ import { setupSwagger } from './util/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
     credentials: true,
   });
   app.useGlobalPipes(
