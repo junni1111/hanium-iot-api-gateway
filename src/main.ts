d65dfac3c95f727271db9cf28063825c6ab94172
김재한 <kimjaehan@gimjaehan-ui-MacBookAir.local>
2022-07-19 13:48:04 +0900

    되돌리기

--- src/main.ts
+++ src/main.ts
@@ -12,13 +12,7 @@ import { setupSwagger } from './util/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    credentials: true,
-  });
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
