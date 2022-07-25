6a544304282a543fe31c0ecd676272c46c78c938
junni1111 <wkszlf98@naver.com>
2022-07-25 17:23:11 +0900

    main.ts에 configService 추가

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  DEVICE_HOST,
-  DEVICE_PORT,
-  GATEWAY_HOST,
-  GATEWAY_PORT,
-} from './config/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { setupSwagger } from './util/swagger';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const DEVICE_HOST = configService.get<string>('DEVICE_HOST');
+  const DEVICE_PORT = configService.get<number>('DEVICE_PORT');
+  const GATEWAY_HOST = configService.get<string>('GATEWAY_HOST');
+  const GATEWAY_PORT = configService.get<number>('GATEWAY_PORT', 7777);
+
   app.enableCors({
     origin: true,
     credentials: true,
@@ -32,7 +33,7 @@ async function bootstrap() {
     `geteway ENV:${process.env.NODE_ENV} - rest host ${GATEWAY_HOST}, rest PORT ${GATEWAY_PORT}, device HOST ${DEVICE_HOST} device PORT ${DEVICE_PORT}`,
   );
 
-  console.log(`ENV List: `, process.env);
+  //console.log(`ENV List: `, process.env);
   await app.listen(GATEWAY_PORT);
 }
 
