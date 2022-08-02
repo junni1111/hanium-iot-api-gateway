732f7703ab795b6d7c772f2a68ff308c279f38d5
junni1111 <wkszlf98@naver.com>
2022-08-02 17:37:57 +0900

    fix : device RPC 포트 삭제

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import { setupSwagger } from './util/swagger';
 import { ConfigService } from '@nestjs/config';
-import { AuthGuard } from './api/user/guards/auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const DEVICE_HOST = configService.get<string>('DEVICE_HOST');
-  const DEVICE_PORT =
-    configService.get<number>('DEVICE_PORT_8888_TCP_PORT') ||
-    configService.get<number>('DEVICE_PORT', 8888);
   const GATEWAY_HOST = configService.get<string>('GATEWAY_HOST');
   const GATEWAY_PORT = configService.get<number>('GATEWAY_PORT', 7777);
 
@@ -28,14 +23,12 @@ async function bootstrap() {
     }),
   );
 
-
-
   app.use(cookieParser());
 
   setupSwagger(app);
 
   console.log(
-    `geteway ENV:${process.env.NODE_ENV} - rest host ${GATEWAY_HOST}, rest PORT ${GATEWAY_PORT}, device HOST ${DEVICE_HOST} device PORT ${DEVICE_PORT}`,
+    `geteway ENV:${process.env.NODE_ENV} - rest host ${GATEWAY_HOST}, rest PORT ${GATEWAY_PORT}`,
   );
 
   console.log(`ENV List: `, process.env);
