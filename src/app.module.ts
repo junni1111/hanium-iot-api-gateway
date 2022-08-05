6ee0e5094a6fafb35ff7a045b72ada118335bd8d
junni1111 <wkszlf98@naver.com>
2022-08-05 19:41:35 +0900

    add : DB synchronize

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiModule } from './api/api.module';
+import { DataBaseConfigModule } from './config/database/database.module';
+import { DataBaseConfigService } from './config/database/database.service';
 import { HealthController } from './health.controller';
-import { APP_GUARD } from '@nestjs/core';
-import { AuthGuard } from './api/user/guards/auth.guard';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `${__dirname}/../env/.env.${process.env.NODE_ENV}`,
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [DataBaseConfigModule],
+      useClass: DataBaseConfigService,
+      inject: [DataBaseConfigService],
     }),
     ApiModule,
   ],
