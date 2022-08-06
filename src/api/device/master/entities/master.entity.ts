3f3b7e77bb2de119afa41e6552ea2ddbd99671e3
KGESH <seonghyeon.jee@gmail.com>
2022-08-06 11:02:43 +0900

    CI: Fix conflict

--- src/api/device/master/entities/master.entity.ts
+++ src/api/device/master/entities/master.entity.ts
@@ -1,17 +1,22 @@
 import {
-  Entity,
   Column,
   CreateDateColumn,
+  Entity,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
-import { IsDate, IsNumber, IsString } from 'class-validator';
+import { Slave } from '../../slave/entities/slave.entity';
 import { ApiProperty } from '@nestjs/swagger';
+import { IsDate, IsNumber, IsString } from 'class-validator';
 
-@Entity({ name: 'masters' })
+@Entity('masters')
 export class Master {
-  @ApiProperty({ example: 1234, description: 'Master id' })
   @PrimaryGeneratedColumn({ type: 'integer', name: 'id' })
+  id: number;
+
+  @ApiProperty({ example: 1234, description: 'Master id' })
   @IsNumber()
+  @Column({ unique: true, name: 'master_id' })
   masterId: number;
 
   @ApiProperty({ example: 'example', description: 'Example address' })
@@ -23,4 +28,9 @@ export class Master {
   @CreateDateColumn({ type: 'timestamptz', name: 'create_at' })
   @IsDate()
   createAt: Date;
+
+  @OneToMany((type) => Slave, (slave) => slave.master, {
+    cascade: ['insert', 'update'],
+  })
+  slaves: Slave[];
 }
